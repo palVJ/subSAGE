bdf35ced739f0083e7824be270f2408884dbeada
palVJ <paal.v.johnsen@gmail.com>
2021-09-14 14:31:11 +0200

    Rcpp code for fast computations

--- f_trees_fast.cpp
+++ f_trees_fast.cpp
@@ -5,15 +5,6 @@ using namespace Rcpp;
 #include <Rinterface.h>
 #endif
 // [[Rcpp::plugins(cpp11)]]
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
 
 
 
